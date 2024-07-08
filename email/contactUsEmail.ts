37a2288e40513b620c10c1e31fa4b738f1b77d9d
Joseph <gadiazajoseph18@gmail.com>
2024-07-08 20:30:27 +0800

    change the variable name in the env file.

--- email/contactUsEmail.ts
+++ email/contactUsEmail.ts
@@ -1,13 +1,12 @@
 import nodemailer from "nodemailer";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
 
-const adminEmail = process.env.NEXT_PUBLIC_ADMIN_EMAIL;
-const adminEmailFull = process.env.NEXT_PUBLIC_ADMIN_EMAIL_FULL;
-const pass = process.env.NEXT_PUBLIC_EMAIL_PASSWORD;
+const adminEmail = process.env.ADMIN_EMAIL;
+const pass = process.env.EMAIL_PASSWORD;
 
 const transportOptions: SMTPTransport.Options = {
-  host: process.env.NEXT_PUBLIC_EMAIL_HOST,
-  port: parseInt(process.env.NEXT_PUBLIC_EMAIL_PORT || "465"),
+  host: process.env.EMAIL_HOST,
+  port: parseInt(process.env.EMAIL_PORT || "465"),
   secure: true,
   auth: {
     user: adminEmail,
